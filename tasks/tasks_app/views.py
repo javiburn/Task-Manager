29f22c5303b23d4c39c48b3f2f3c31b80c30d5c6
Javier Sarabia Rodriguez <jsarabia@c3r12s1.42madrid.com>
2024-07-09 17:09:58 +0200

    store session information

--- tasks/tasks_app/views.py
+++ tasks/tasks_app/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
-tasks = ["foo", "bar", "baz"]
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+
 # Create your views here.
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
     return render(request, "index.html", {
-        "tasks": tasks
+        "tasks": request.session["tasks"]
+    })
+
+def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            request.session["tasks"] += [task]
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "add.html", {
+                "form": form
+            })
+    return render(request, "add.html", {
+        "form": NewTaskForm()
     })
